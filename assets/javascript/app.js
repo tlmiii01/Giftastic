67986131c88e500f84b1162911a8a264a88065fd
Thomas Martin <tmartin.dev101010@gmail.com>
2018-09-05 12:29:08 -0600

    Complete function to build the query string.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,19 @@
 var animals = ["cats", "dogs"];
+var searchTerm;
+var numberOfGifs = 10;
+
+// Variables for API calls
+
+function buildQuery() {
+    var apiKey = "HotDTaBju1BKdzeosh4NfXZVRsKBVo2s";
+    var baseURL = "http://api.giphy.com/v1/gifs/search";
+
+    var newQuery = baseURL + "?api_key=" + apiKey;
+    newQuery += "&q=" + searchTerm;
+    newQuery += "&limit=" + numberOfGifs;
+
+    return newQuery;
+}
 
 function addSearchButton(animal) {
      
@@ -31,11 +46,14 @@ $(document).ready(function() {
             console.log(newAnimal);
             addSearchButton(newAnimal);
         }
-    })
+    });
 
     $("#button-row").on("click", ".animal-button", function() {
-        console.log($(this));
-        console.log($(this).attr("data-name"));
-    })
+        // console.log($(this));
+        // console.log($(this).attr("data-name"));
+        searchTerm = $(this).attr("data-name");
+        console.log(buildQuery());
+
+    });
 
 });
