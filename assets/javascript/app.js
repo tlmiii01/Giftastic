ca187b7c6ed0a61e888b02c8aae2bf20459d4f72
Thomas Martin <tmartin.dev101010@gmail.com>
2018-09-05 19:51:00 -0600

    Loading 10 gifs is working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,19 +16,19 @@ function buildQuery() {
 }
 
 function addSearchButton(animal) {
-     
+
     var newButton = $("<button>");
     newButton.addClass("btn btn-success animal-button");
     newButton.attr("data-name", animal);
     newButton.text(animal);
     $("#button-row").append(newButton);
-    
+
 }
 
 function loadButtons() {
     // Remove everything in button-row
     $("#button-row").empty();
-    
+
     for (let i = 0; i < animals.length; ++i) {
         addSearchButton(animals[i]);
     }
@@ -36,60 +36,54 @@ function loadButtons() {
 
 //Function to build a card with the GIF
 function buildGifCard(gif) {
-//     <div class="card" style="width: 18rem;">
-//   <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     <a href="#" class="btn btn-primary">Go somewhere</a>
-//   </div>
-// </div>
 
-var newCard = $("<div>");
-newCard.addClass("card");
-var tempDiv = $("<img>");
-tempDiv.addClass("card-img-top");
-tempDiv.attr("src", gif.images.fixed_width_small.url);
-console.log(gif.images.fixed_width_small.url);
-newCard.append(tempDiv);
+    var newCard = $("<div>");
+    newCard.addClass("card col-sm-4");
+    var tempDiv = $("<img>");
+    tempDiv.addClass("card-img-top");
+    tempDiv.attr("src", gif.images.fixed_height_still.url);
+    console.log(gif.images.fixed_height_still.url);
+    newCard.append(tempDiv);
 
-tempDiv = $("<div>");
-tempDiv.addClass("card-body");
-tempDiv.append("<h5 class='card-title'>" + gif.rating + "</h5>");
+    tempDiv = $("<div>");
+    tempDiv.addClass("card-body");
+    tempDiv.append("<h5 class='card-title'>" + gif.rating + "</h5>");
 
-newCard.append(tempDiv);
-return newCard;
-    
+    newCard.append(tempDiv);
+    return newCard;
+
 }
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     loadButtons();
-    
+
     $("#submitButton").on("click", (event) => {
         event.preventDefault();
-       
+
         var newAnimal = $("#newItem").val();
-        if ( newAnimal != "" ) {
+        if (newAnimal != "") {
             console.log(newAnimal);
             addSearchButton(newAnimal);
         }
     });
 
-    $("#button-row").on("click", ".animal-button", function() {
+    $("#button-row").on("click", ".animal-button", function () {
         // console.log($(this));
         // console.log($(this).attr("data-name"));
         searchTerm = $(this).attr("data-name");
         // console.log(buildQuery());
-        
+
         // AjaX call to get the GIFs
         $.ajax({
             url: buildQuery(),
             method: "GET"
-        }).then( (response) => {
-            console.log(response);
-            $("#gifImages").append(buildGifCard(response.data[0]));
-        })
+        }).then((response) => {
+            $("#gifImages").empty();
+            for (let i = 0; i < numberOfGifs; i++) {
+                $("#gifImages").append(buildGifCard(response.data[i]));
+            }
+        });
     });
 
 
